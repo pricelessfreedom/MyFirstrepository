c78da6b9163416ff011a5df97e512d1e1cc590cd
John <johndoe@example.com>
2019-05-17 13:59:26 +0800

    onStart

--- CoolWeather/app/src/main/java/com/coolweather/android/MainActivity.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/MainActivity.java
@@ -27,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 	
+	
+	@Override
+    protected void onStart() {
+        super.onStart();
+    }
+	
 	@Override
     protected void onDestroy() {
         super.onDestroy();
